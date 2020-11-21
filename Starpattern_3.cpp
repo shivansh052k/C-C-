e6ea166969c6cdd8bb4a39efef7dcbcb10800aad
Shivansh Gupta <shivanshgupta323@gmail.com>
2020-11-21 23:19:48 +0530

    Add files via upload

---
+++ Starpattern_3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    /*
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    */
+    int n;
+    cin>>n;
+
+    for (int i=n; i>=1; i--)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            cout<<"*";      
+        }
+        cout<<endl;
+    }
+    
+}
