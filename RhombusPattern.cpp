63dc2d674a2ac73d3a8afe4cc2f9f1b4b6f52d25
Shivansh Gupta <shivanshgupta323@gmail.com>
2020-11-21 23:16:53 +0530

    Add files via upload

---
+++ RhombusPattern.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+    /*
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+    */
+    int n;
+    cin>>n;
+    
+    for (int i=1; i<=n; i++)
+    {
+        for(int j=0; j<=n-i; j++)
+        {
+            cout<<" ";
+        }
+        for(int j=0; j<=n; j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
